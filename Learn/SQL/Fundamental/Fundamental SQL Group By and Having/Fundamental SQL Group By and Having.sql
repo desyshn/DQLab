e7f25611accff0a6da9faa59faf2d66ee9164e88
desyshn <32958641+desyshn@users.noreply.github.com>
2021-10-28 21:35:51 +0700

    Create Fundamental SQL Group By and Having.sql

---
+++ Learn/SQL/Fundamental/Fundamental SQL Group By and Having/Fundamental SQL Group By and Having.sql
@@ -0,0 +1,16 @@
+USE dqlab;
+
+-- PPengenalan Table - Customer
+SELECT *FROM customer;
+
+-- Pengenalan Table - Product
+SELECT *FROM product;
+
+-- Pengenalan Table - Subscription
+SELECT *FROM subscription;
+
+-- Pengenalan Table - Invoice
+SELECT *FROM invoice;
+
+-- Pengenalan Table - Payment
+SELECT *FROM payment;
