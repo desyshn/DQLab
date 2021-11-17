d4b2aae55aad586a3be4ffe2b34802512f85f645
desyshn <32958641+desyshn@users.noreply.github.com>
2021-11-17 22:03:23 +0700

    Create Introduction to Data Science with R.R

---
+++ Learn/R/Preparation/Introduction to Data Science with R.R
@@ -0,0 +1,21 @@
+#"Hello World" di R
+"Hello World"
+
+#Praktek: Perhitungan Sederhana
+10+7
+
+#Variabel dalam R
+a=5
+print(a)
+
+#Praktek: Penggunaan Comment
+10 + 7 #Ini adalah baris komentar
+
+#Praktek: Penggunaan Vector - Bagian Satu
+c(3, 10, 15)
+
+#Praktek: Penggunaan Vector - Bagian Dua
+c(1:5)
+
+#Menggunakan Fungsi Summary
+summary(c(1:5))
